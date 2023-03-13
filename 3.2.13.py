48e4b8656817f4caef01985f0dd395aebbcea5c6
Dimitriy Kanaev <72267931+tuesvon@users.noreply.github.com>
2023-03-13 15:06:55 +0300

    Добавление лекций и дз модуля 3 главы 2

---
+++ 3.2.13.py
@@ -0,0 +1,51 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+import unittest
+from self import self
+
+
+class test_RegForm(unittest.TestCase):
+    def test_Reg1(link1):
+        link1 = "http://suninjuly.github.io/registration1.html"
+        browser = webdriver.Chrome()
+        browser.get(link1)
+        input1 = browser.find_element(By.CLASS_NAME, "form-control.first:required")
+        input1.send_keys("Dimitrii")
+        input2 = browser.find_element(By.CLASS_NAME, "form-control.second:required")
+        input2.send_keys("Kanaev")
+        input3 = browser.find_element(By.CLASS_NAME, "form-control.third:required")
+        input3.send_keys("wwas@wwas.wwas")
+        input4 = browser.find_element(By.XPATH, "//div[2]/div[1]/input")
+        input4.send_keys("+79909664456")
+        input5 = browser.find_element(By.XPATH, "//div[2]/div[2]/input")
+        input5.send_keys("Saint-P")
+        button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+        button.click()
+        browser.quit()
+
+    def test_Reg2(link2):
+        link2 = "http://suninjuly.github.io/registration2.html"
+        browser = webdriver.Chrome()
+        browser.get(link2)
+        input1 = browser.find_element(By.CLASS_NAME, "form-control.first:required")
+        input1.send_keys("Dimitrii")
+        input2 = browser.find_element(By.CLASS_NAME, "form-control.second:required")
+        input2.send_keys("Kanaev")
+        input3 = browser.find_element(By.CLASS_NAME, "form-control.third:required")
+        input3.send_keys("wwas@wwas.wwas")
+        input4 = browser.find_element(By.XPATH, "//div[2]/div[1]/input")
+        input4.send_keys("+79909664456")
+        input5 = browser.find_element(By.XPATH, "//div[2]/div[2]/input")
+        input5.send_keys("Saint-P")
+        button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+        button.click()
+        browser.quit()
+
+
+def assertEqual():
+    self.assertEqual(test_RegForm)
+
+
+if __name__ == '__main__':
+    unittest.main()
