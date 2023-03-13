5136f005f19f8b614af2d1a06ef5b81af6d0c024
Dimitriy Kanaev <72267931+tuesvon@users.noreply.github.com>
2023-03-13 13:01:20 +0300

    Добавление лекций и дз из модуля 3

---
+++ 3.2.6.py
@@ -0,0 +1,15 @@
+#assert abs(-42) == 42
+
+#assert abs(-42) == -42, "Целое положительное число"
+
+#print("Let's count together: {}, then goes {}, and then {}".format("one", "two", "three"))
+
+#str1 = "one"
+#str2 = "two"
+#str3 = "three"
+#print(f"Let's count together: {str1}, then goes {str2}, and then {str3}")
+
+#actual_result = "abrakadabra"
+#print(f"Wrong text, got {actual_result}, something wrong")
+
+#print(f"{2+3}")
